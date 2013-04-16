bfb651661e06bc7a08251f84e3d72a49368e496b
Micah Wylde <micah@micahw.com>
2013-04-16 01:28:03 -0700

    Now displays time, various graphical improvements

--- src/timer.c
+++ src/timer.c
@@ -8,9 +8,19 @@
 #define MY_UUID { 0x61, 0x8C, 0xA5, 0x8D, 0xC0, 0xEB, 0x49, 0xDB, 0x98, 0x56, 0x03, 0x40, 0x36, 0xAE, 0xBC, 0x45 }
 PBL_APP_INFO(MY_UUID,
              "Timer", "Micah Wylde",
-             0, 2, /* App version */
+             0, 3, /* App version */
              RESOURCE_ID_IMAGE_MENU_ICON,
              APP_INFO_STANDARD_APP);
+
+#define WHITE_ON_BLACK
+
+#ifndef WHITE_ON_BLACK
+#define COLOR_FOREGROUND GColorBlack
+#define COLOR_BACKGROUND GColorWhite
+#else
+#define COLOR_FOREGROUND GColorWhite
+#define COLOR_BACKGROUND GColorBlack
+#endif
 
 enum State {
   DONE,
@@ -23,7 +33,9 @@ enum State {
 // Main window stuff
 Window window;
 
+TextLayer title;
 TextLayer count_down;
+TextLayer time_text;
 
 enum State current_state = DONE;
 
@@ -46,19 +58,19 @@ enum SettingUnit {
 
 enum SettingUnit setting_unit = SETTING_MINUTE;
 
-void update_time() {
+void update_countdown() {
   if (current_seconds == last_set_time) {
     return;
   }
 
-  static char time_text[] = "00:00:00";
+  static char time_text[] = " 00:00:00";
 
   PblTm time;
   time.tm_hour = current_seconds / (60 * 60);
   time.tm_min  = (current_seconds - time.tm_hour * 60 * 60) / 60;
   time.tm_sec  = current_seconds - time.tm_hour * 60 * 60 - time.tm_min * 60;
 
-  string_format_time(time_text, sizeof(time_text), "%T", &time);
+  string_format_time(time_text, sizeof(time_text), " %T", &time);
 
   text_layer_set_text(&count_down, time_text);
 
@@ -79,7 +91,7 @@ void toggle_setting_mode(ClickRecognizerRef recognizer, Window *window) {
   }
   else {
     current_seconds = total_seconds;
-    update_time();
+    update_countdown();
     current_state = SETTING;
     setting_unit = SETTING_MINUTE;
     draw_setting_unit();
@@ -93,7 +105,7 @@ void unit_marker_update_callback(Layer *me, GContext* ctx) {
   int start = 8 + (width + 14) * setting_unit;
 
   if (current_state == SETTING && setting_blink_state) {
-    graphics_context_set_stroke_color(ctx, GColorBlack);
+    graphics_context_set_stroke_color(ctx, COLOR_FOREGROUND);
 
     for (int i = 0; i < 4; i++) {
       graphics_draw_line(ctx, GPoint(start, 95 + i), GPoint(start + width, 95 + i));
@@ -132,7 +144,7 @@ void increment_time(int direction) {
     if (new_seconds >= 0 && new_seconds < 100 * 60 * 60) {
       total_seconds = new_seconds;
       current_seconds = total_seconds;
-      update_time();
+      update_countdown();
     }
   }
 }
@@ -150,7 +162,7 @@ void reset_timer(ClickRecognizerRef recognizer, Window *window) {
   if (current_state != SETTING) {
     current_state = DONE;
     current_seconds = total_seconds;
-    update_time();
+    update_countdown();
   }
 }
 
@@ -187,7 +199,7 @@ void main_click_provider(ClickConfig **config, Window *window) {
 void handle_second_counting_down() {
   current_seconds--;
 
-  update_time();
+  update_countdown();
 
   if (current_seconds == 0) {
     vibes_enqueue_custom_pattern(alarm_finished);
@@ -197,13 +209,28 @@ void handle_second_counting_down() {
 
 void handle_second_waiting() {
   current_seconds = total_seconds;
-  update_time();
+  update_countdown();
 }
-
 
 void handle_second_setting() {
   setting_blink_state = !setting_blink_state;
   layer_mark_dirty(&unit_marker);
+}
+
+void update_time(PblTm *tick_time) {
+  static char time[] = "Xxxxxxxxx - 00 00:00";
+
+  char *time_format;
+
+  if (clock_is_24h_style()) {
+    time_format = "%B %e   %R";
+  } else {
+    time_format = "%B %e   %I:%M";
+  }
+
+  string_format_time(time, sizeof(time), time_format, tick_time);
+
+  text_layer_set_text(&time_text, time);
 }
 
 void handle_second_tick(AppContextRef ctx, PebbleTickEvent *t) {
@@ -220,6 +247,10 @@ void handle_second_tick(AppContextRef ctx, PebbleTickEvent *t) {
   default:
     break;
   }
+
+  if (t->units_changed & MINUTE_UNIT) {
+    update_time(t->tick_time);
+  }
 }
 
 void handle_init(AppContextRef ctx) {
@@ -230,19 +261,39 @@ void handle_init(AppContextRef ctx) {
   window_init(&window, "Main Window");
   window_stack_push(&window, true /* Animated */);
 
+  window_set_background_color(&window, COLOR_BACKGROUND);
+  window_set_fullscreen(&window, true);
   window_set_click_config_provider(&window, (ClickConfigProvider) main_click_provider);
 
   GFont custom_font = \
     fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_32));
 
-  text_layer_init(&count_down, GRect(7, 54, 144, 168-54));
+  text_layer_init(&count_down, GRect(0, 62, 144, 40));
   text_layer_set_font(&count_down, custom_font);
-  update_time();
+  text_layer_set_text_color(&count_down, COLOR_BACKGROUND);
+  text_layer_set_background_color(&count_down, COLOR_FOREGROUND);
+  update_countdown();
   layer_add_child(&window.layer, &count_down.layer);
 
   layer_init(&unit_marker, window.layer.frame);
   unit_marker.update_proc = unit_marker_update_callback;
   layer_add_child(&window.layer, &unit_marker);
+
+  text_layer_init(&title, GRect(30, 5, 100, 24));
+  text_layer_set_font(&title, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  text_layer_set_text_color(&title, COLOR_FOREGROUND);
+  text_layer_set_background_color(&title, GColorClear);
+  text_layer_set_text(&title, "pebble timer");
+  layer_add_child(&window.layer, &title.layer);
+
+  text_layer_init(&time_text, GRect(20, 130, 110, 24));
+  text_layer_set_font(&time_text, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  text_layer_set_text_color(&time_text, COLOR_FOREGROUND);
+  text_layer_set_background_color(&time_text, GColorClear);
+  PblTm time;
+  get_time(&time);
+  update_time(&time);
+  layer_add_child(&window.layer, &time_text.layer);
 }
 
 
@@ -257,3 +308,4 @@ void pbl_main(void *params) {
   };
   app_event_loop(params, &handlers);
 }
+
